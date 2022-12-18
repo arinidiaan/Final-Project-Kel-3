5b2258829f20ed877fd5af35bf1cedcd4a5a6600
dianarini <arinidiaan@gmail.com>
2022-12-18 17:45:31 +0700

    update folder

--- Recruitment/AddCandidate.py
+++ Recruitment/AddCandidate.py
@@ -27,45 +27,7 @@ class TestRecruitment(unittest.TestCase):
         browser.find_element(By.XPATH,"//div[@id='app']/div[@class='orangehrm-login-layout']/div[@class='orangehrm-login-layout-blob']//form[@action='/web/index.php/auth/validate']/div[3]/button[@type='submit']").click()
         time.sleep(3)
 
-    # def toRecruitmentMenu(self) :
-    #     browser = self.browser
-    #     browser.find_element(By.XPATH,"//div[@id='app']//aside[@class='oxd-sidepanel']/nav[@role='navigation']//ul[@class='oxd-main-menu']//a[@href='/web/index.php/recruitment/viewRecruitmentModule']/span[.='Recruitment']").click()
-    #     time.sleep()
-
-    def input_field(browser) :
-        #check first name field
-        WebDriverWait(browser, 20).until(EC.presence_of_element_located(((By.XPATH,"//div[@id='app']//form[@class='oxd-form']/div[1]/div/div[@class='oxd-grid-item oxd-grid-item--gutters']/div[@class='oxd-input-group']/div[@class='--name-grouped-field']/div[1]//input[@name='firstName']")))).send_keys("Jennie")
-
-        #check middle name field
-        WebDriverWait(browser, 20).until(EC.presence_of_element_located(((By.XPATH,"//div[@id='app']//form[@class='oxd-form']/div[1]/div/div[@class='oxd-grid-item oxd-grid-item--gutters']/div[@class='oxd-input-group']/div[@class='--name-grouped-field']/div[2]//input[@name='middleName']")))).send_keys("Ruby")
-
-        #check last name field
-        WebDriverWait(browser, 20).until(EC.presence_of_element_located(((By.XPATH,"//div[@id='app']//form[@class='oxd-form']/div[1]/div/div[@class='oxd-grid-item oxd-grid-item--gutters']/div[@class='oxd-input-group']/div[@class='--name-grouped-field']/div[3]//input[@name='lastName']")))).send_keys("Jane")
-
-        #check dropdown menu
-        WebDriverWait(browser, 20).until(EC.presence_of_element_located(((By.XPATH,"//div[@id='app']/div[@class='oxd-layout']//form[@class='oxd-form']/div[2]/div/div[@class='oxd-grid-item oxd-grid-item--gutters']/div//div[@class='oxd-select-wrapper']/div")))).click()
-        # dropdown_menu = browser.find_element(By.XPATH,"//div[@id='app']/div[@class='oxd-layout']//form[@class='oxd-form']/div[2]/div/div[@class='oxd-grid-item oxd-grid-item--gutters']/div//div[@class='oxd-select-wrapper']/div[@class='oxd-select-dropdown --position bottom']/div[5]").click()
-        # browser.find_element(By.XPATH,"//div[@id='app']//form[@class='oxd-form']/div[2]/div/div[@class='oxd-grid-item oxd-grid-item--gutters']/div//div[@class='oxd-select-wrapper']/div/div[@class='oxd-select-text-input']").send_keys("Senior QA Lead")
-        # time.sleep(3)
-
-        #select menu
-        option = browser.find_elements(By.XPATH,"//div[@id='app']//div[@class='oxd-table-filter']/div[@class='oxd-table-filter-area']/form[@class='oxd-form']/div[2]/div/div//div[@class='oxd-select-wrapper']/div[2]/div[@class='oxd-select-option']/span")
-        
-        for i in range (len(option)) :
-            if option[i].text == "Senior QA Lead" :
-                option[i].click()
-                break
-        time.sleep(3)
-
-        #check email field
-        WebDriverWait(browser, 20).until(EC.presence_of_element_located(((By.XPATH,"//div[@id='app']//form[@class='oxd-form']/div[3]/div/div[1]/div//input[@placeholder='Type here']")))).send_keys("a@example.com")
-        #check contact number field
-        WebDriverWait(browser, 20).until(EC.presence_of_element_located(((By.XPATH,"//div[@id='app']//form[@class='oxd-form']/div[3]/div/div[2]/div//input[@placeholder='Type here']")))).send_keys("085725100620")
-        #check keyword field
-        WebDriverWait(browser, 20).until(EC.presence_of_element_located(((By.XPATH,"//div[@id='app']//form[@class='oxd-form']/div[5]/div/div[1]/div//input[@placeholder='Enter comma seperated words...']")))).send_keys("Engineer")
-        #check Note field
-        WebDriverWait(browser, 20).until(EC.presence_of_element_located(((By.XPATH,"//div[@id='app']//form[@class='oxd-form']/div[6]/div/div/div//textarea[@placeholder='Type here']")))).send_keys("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book")
-        time.sleep(3)
+    
 
     def test_a_add_candidates_field_valid_data(self): 
         # steps
@@ -98,7 +60,7 @@ class TestRecruitment(unittest.TestCase):
         # time.sleep(3)
 
         #select menu
-        option = browser.find_elements(By.XPATH,"//div[@id='app']//div[@class='oxd-table-filter']/div[@class='oxd-table-filter-area']/form[@class='oxd-form']/div[2]/div/div//div[@class='oxd-select-wrapper']/div[2]/div[@class='oxd-select-option']/span")
+        option = browser.find_elements(By.XPATH,"//div[@id='app']//form[@class='oxd-form']/div[2]/div/div[@class='oxd-grid-item oxd-grid-item--gutters']/div//div[@class='oxd-select-wrapper']/div[2]/div[@class='oxd-select-option']/span")
         
         for i in range (len(option)) :
             if option[i].text == "Senior QA Lead" :
@@ -185,33 +147,66 @@ class TestRecruitment(unittest.TestCase):
         browser.find_element(By.XPATH,"//div[@id='app']//aside[@class='oxd-sidepanel']/nav[@role='navigation']//ul[@class='oxd-main-menu']//a[@href='/web/index.php/recruitment/viewRecruitmentModule']/span[.='Recruitment']").click()
         time.sleep(3)
 
+        #check jumlah record
+        def len_rec() :
+            record = browser.find_elements(By.XPATH,"//div[@id='app']/div[@class='oxd-layout']//div[@role='table']/div[2]//div[@role='row']/div[1]/div")
+            print(len(record))
+            return len(record)
+
+
         #check record before add
-        record = browser.find_elements(By.XPATH,"//div[@id='app']/div[@class='oxd-layout']//div[@role='table']/div[2]//div[@role='row']/div[1]/div")
-        len_rec_before_add = len(record)
+        len_rec_before_add = len_rec()
         #click add button
         browser.find_element(By.XPATH,"//div[@id='app']/div[@class='oxd-layout']/div[@class='oxd-layout-container']//div[@class='orangehrm-header-container']/button[@type='button']/i").click() 
         time.sleep(3)
 
         #isi data
-        self.input_field(browser)
+        #check first name field
+        WebDriverWait(browser, 20).until(EC.presence_of_element_located(((By.XPATH,"//div[@id='app']//form[@class='oxd-form']/div[1]/div/div[@class='oxd-grid-item oxd-grid-item--gutters']/div[@class='oxd-input-group']/div[@class='--name-grouped-field']/div[1]//input[@name='firstName']")))).send_keys("Jennie")
+        #check middle name field
+        WebDriverWait(browser, 20).until(EC.presence_of_element_located(((By.XPATH,"//div[@id='app']//form[@class='oxd-form']/div[1]/div/div[@class='oxd-grid-item oxd-grid-item--gutters']/div[@class='oxd-input-group']/div[@class='--name-grouped-field']/div[2]//input[@name='middleName']")))).send_keys("Ruby")
+        #check last name field
+        WebDriverWait(browser, 20).until(EC.presence_of_element_located(((By.XPATH,"//div[@id='app']//form[@class='oxd-form']/div[1]/div/div[@class='oxd-grid-item oxd-grid-item--gutters']/div[@class='oxd-input-group']/div[@class='--name-grouped-field']/div[3]//input[@name='lastName']")))).send_keys("Jane")
+        #check dropdown menu
+        WebDriverWait(browser, 20).until(EC.presence_of_element_located(((By.XPATH,"//div[@id='app']/div[@class='oxd-layout']//form[@class='oxd-form']/div[2]/div/div[@class='oxd-grid-item oxd-grid-item--gutters']/div//div[@class='oxd-select-wrapper']/div")))).click()
+        # dropdown_menu = browser.find_element(By.XPATH,"//div[@id='app']/div[@class='oxd-layout']//form[@class='oxd-form']/div[2]/div/div[@class='oxd-grid-item oxd-grid-item--gutters']/div//div[@class='oxd-select-wrapper']/div[@class='oxd-select-dropdown --position bottom']/div[5]").click()
+
+        #select menu
+        option = browser.find_elements(By.XPATH,"//div[@id='app']//form[@class='oxd-form']/div[2]/div/div[@class='oxd-grid-item oxd-grid-item--gutters']/div//div[@class='oxd-select-wrapper']/div[2]/div[@class='oxd-select-option']/span")
+        
+        for i in range (len(option)) :
+            if option[i].text == "Senior QA Lead" :
+                option[i].click()
+                break
+        time.sleep(3)
+
+        #check email field
+        WebDriverWait(browser, 20).until(EC.presence_of_element_located(((By.XPATH,"//div[@id='app']//form[@class='oxd-form']/div[3]/div/div[1]/div//input[@placeholder='Type here']")))).send_keys("a@example.com")
+        #check contact number field
+        WebDriverWait(browser, 20).until(EC.presence_of_element_located(((By.XPATH,"//div[@id='app']//form[@class='oxd-form']/div[3]/div/div[2]/div//input[@placeholder='Type here']")))).send_keys("085725100620")
+        #check keyword field
+        WebDriverWait(browser, 20).until(EC.presence_of_element_located(((By.XPATH,"//div[@id='app']//form[@class='oxd-form']/div[5]/div/div[1]/div//input[@placeholder='Enter comma seperated words...']")))).send_keys("Engineer")
+        #check Note field
+        WebDriverWait(browser, 20).until(EC.presence_of_element_located(((By.XPATH,"//div[@id='app']//form[@class='oxd-form']/div[6]/div/div/div//textarea[@placeholder='Type here']")))).send_keys("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book")
+        time.sleep(3)
         #click save
         browser.find_element(By.XPATH,"//div[@id='app']/div[@class='oxd-layout']//form[@class='oxd-form']/div[@class='oxd-form-actions']/button[@type='submit']").click()
         time.sleep(3)
+
+        # #cek toast success
+        # toast = browser.find_elements(By.XPATH,"//div[@id='oxd-toaster_1']/div[@class='oxd-toast oxd-toast--success oxd-toast-container--toast']//p[@class='oxd-text oxd-text--p oxd-text--toast-title oxd-toast-content-text']")
+        # self.assertIn("Success", toast[0].text)
+        # print(toast[1].text)
+
+        #go to view candidate to check row bertambah
+        browser.get("https://opensource-demo.orangehrmlive.com/web/index.php/recruitment/viewCandidates")
+        time.sleep(5)
         #check record bertambah
-        record = browser.find_elements(By.XPATH,"//div[@id='app']/div[@class='oxd-layout']//div[@role='table']/div[2]//div[@role='row']/div[1]/div")
-        len_rec_after_add = len(record)
-        self.assertTrue(len_rec_after_add == len_rec_after_add+1,"Record is not added")
+        len_rec_after_add = len_rec()
+        self.assertIn("("+(str(len_rec_after_add))+") Records Found",browser.find_element(By.XPATH,"//div[@id='app']/div[@class='oxd-layout']/div[@class='oxd-layout-container']/div[@class='oxd-layout-context']//div[@class='orangehrm-horizontal-padding orangehrm-vertical-padding']/span[@class='oxd-text oxd-text--span']").text)
 
-        self.assertIn("("+(str(len(record)))+") Records Found",browser.find_element(By.XPATH,"//div[@id='app']/div[@class='oxd-layout']/div[@class='oxd-layout-container']/div[@class='oxd-layout-context']//div[@class='orangehrm-horizontal-padding orangehrm-vertical-padding']/span[@class='oxd-text oxd-text--span']").text)
-
-
-        #check after save redirect to previous page
-        get_url = browser.current_url
-        self.assertEqual(get_url, 'https://opensource-demo.orangehrmlive.com/web/index.php/recruitment/addCandidate')
         
         
-
-
 
 
     def tearDown(self): 
